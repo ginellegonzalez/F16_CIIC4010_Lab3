f92ff7cd47d438b9995aa0d07ae17eb6d0a45b73
4015b8 <4015b8@icaco.ece.uprm.edu>
2016-08-31 11:27:01 -0400

    Mid bandera

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("GINELLE");
 		//myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(800, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(250, 178);
 		
 	    MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -19,7 +19,7 @@ public class MyPanelClass extends JPanel {
 		int height = y2 - y1;
 
 		//Paint the background
-		g.setColor(Color.CYAN);
+		g.setColor(Color.WHITE);
 		g.fillRect(x1, y1, width+1, height+1);
 
 		//                        //Draw a border
@@ -52,6 +52,31 @@ public class MyPanelClass extends JPanel {
 //						           p.addPoint(x1 + 15, y1 + 25);
 //						           g.setColor(Color.YELLOW);
 //						           g.fillPolygon(p);
+		
+									//franjas ROJAS
+									Polygon p4 = new Polygon();
+									p4.addPoint(x1, y1);
+									p4.addPoint(x1, y1+37);
+									p4.addPoint(x1+250, y1+37);
+									p4.addPoint(x1+250, y1);
+									g.setColor(Color.RED);
+									g.fillPolygon(p4);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+									//triangle 
+									Polygon p3 = new Polygon();
+									p3.addPoint(x1, y1);
+									p3.addPoint(x1+125, y1+89);
+									p3.addPoint(x1, y1+178);
+									g.setColor(Color.BLUE);
+									g.fillPolygon(p3);
 									
 									Polygon p2 = new Polygon();
 							        p2.addPoint(x1 + 25, y1 + 73);
@@ -65,6 +90,6 @@ public class MyPanelClass extends JPanel {
 							        p2.addPoint(x1 + 34, y1 + 98);
 							        p2.addPoint(x1 + 38, y1 + 83);
 							        g.setColor(Color.WHITE);
-							        g.drawPolygon(p2);
+							        g.fillPolygon(p2);
 	}
 }
