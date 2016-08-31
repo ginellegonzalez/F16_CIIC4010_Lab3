9138aae3738219dee140a1ccd781efbc73a6037d
4015b8 <4015b8@icaco.ece.uprm.edu>
2016-08-31 10:25:29 -0400

    Changed the color of the background, added two borders.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,14 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("GINELLE GONZALEZ");
+		JFrame myFrame = new JFrame("GINELLE");
 		//myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(800, 400);
 		myFrame.setSize(200, 200);
+		
+	    MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+        
 		myFrame.setVisible(true);
 		
 	}
