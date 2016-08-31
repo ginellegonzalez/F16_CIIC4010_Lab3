0ed3d624ba20d8d71bf0c3ce39843fe4825ae3a8
4015b8 <4015b8@icaco.ece.uprm.edu>
2016-08-31 10:47:59 -0400

    Added a centered circle.

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -20,12 +20,25 @@ public class MyPanelClass extends JPanel {
                         g.setColor(Color.CYAN);
                         g.fillRect(x1, y1, width+1, height+1);
                         
-                        //Draw a border
-                        g.setColor(Color.MAGENTA);
-                        g.drawRect(x1, y1, width, height);
+//                        //Draw a border
+//                        g.setColor(Color.MAGENTA);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        //inside border
+//                        g.setColor(Color.DARK_GRAY);
+//                        g.drawRect(x1+2, y1+2, width-4, height-4);
+//                        
+//                        //diagonal line from left
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        //diagonal line from right
+//                        g.setColor(Color.ORANGE);
+//                        g.drawLine(x2, y1, x1, y2);
                         
-                        g.setColor(Color.DARK_GRAY);
-                        g.drawRect(x1+2, y1+2, width-4, height-4);
+                        //OVAL
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval((width/2)-27,(height/2)-27, 55, 55);
                         
                         
             }
